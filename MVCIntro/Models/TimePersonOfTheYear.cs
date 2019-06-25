c16c1eee18e9d17eaae502d3aebaca2f1eff76c2
Jon Rice <revyolution1120@gmail.com>
2019-06-25 09:18:44 -0700

    Start for logic for getting file into app. Need to add csv file.

--- MVCIntro/Models/TimePersonOfTheYear.cs
+++ MVCIntro/Models/TimePersonOfTheYear.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -17,6 +18,19 @@ namespace MVCIntro.Models
 
         public static List<TimePersonOfTheYear> GetPeople(int firstYear, int secondYear)
         {
+            List<TimePersonOfTheYear> people = new List<TimePersonOfTheYear>();
+
+            // Set path to grab file to current directory.
+            string filePath = Environment.CurrentDirectory;
+            // Set new path
+            string newPath = Path.GetFullPath(Path.Combine(filePath, @"wwwroot\personOfTheYear.csv"));
+            // Read CSV file into a string array.
+            string[] allPeople = File.ReadAllLines(newPath);
+
+            for (int i = 0; i < allPeople.Length; i++)
+            {
+                string[] items = allPeople[i].Split(',');
+            }
             return peopleList;
         }
     }
